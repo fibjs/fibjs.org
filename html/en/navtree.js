96e1cbab5f4dbf337d37f8aa617834c6498ea425
响马 <lion@9465.net>
2015-01-22 12:34:50 +0800

    升级 doxygen 重新生成文档

--- html/en/navtree.js
+++ html/en/navtree.js
@@ -1,60 +1,3 @@
-var NAVTREE =
-[
-  [ "Javascript on Fiber", "index.html", [
-    [ "Quick Start", "index.html", [
-      [ "What’s fibjs", "index.html#intro_sec", null ],
-      [ "Build release", "index.html#install_sec", [
-        [ "Step 1: Preparation for Building", "index.html#install_step1", null ],
-        [ "Step 2: Fetch source code", "index.html#install_step2", null ],
-        [ "Step 3: Building", "index.html#install_step3", null ],
-        [ "Step 4: Test cases", "index.html#install_step4", null ],
-        [ "Step 5: Have a rest and drinking", "index.html#install_step5", null ]
-      ] ]
-    ] ],
-    [ "Beginning", "d2/de8/start_prog.html", [
-      [ "Beginning", "d2/de8/start_prog.html#program_sec", [
-        [ "Step 1: Hello, World!", "d2/de8/start_prog.html#prg_step1", null ],
-        [ "Step 2: DANG, DANG, DANG...", "d2/de8/start_prog.html#prg_step2", null ],
-        [ "Step 3: DANGs", "d2/de8/start_prog.html#prg_step3", null ],
-        [ "Advance: Parallel Jobs", "d2/de8/start_prog.html#prg_step4", null ],
-        [ "More Advance: Asynchronous Jobs", "d2/de8/start_prog.html#prg_step5", null ],
-        [ "Summary", "d2/de8/start_prog.html#prg_step10", null ]
-      ] ]
-    ] ],
-    [ "Module Management", "d0/db5/module.html", [
-      [ "Basic Elements", "d0/db5/module.html#element", [
-        [ "require() Function", "d0/db5/module.html#fun_require", null ],
-        [ "define() Function", "d0/db5/module.html#fun_define", null ],
-        [ "module Object", "d0/db5/module.html#obj_module", null ],
-        [ "exports Object", "d0/db5/module.html#obj_exports", null ]
-      ] ]
-    ] ],
-    [ "Server Programming", "da/db3/server.html", [
-      [ "Static File Server", "da/db3/server.html#web_file", null ],
-      [ "Dynamic Server", "da/db3/server.html#web_script", null ],
-      [ "Both Static and Dynamic Supoort", "da/db3/server.html#web_both", null ]
-    ] ],
-    [ "Module", "namespaces.html", "namespaces" ],
-    [ "Object", "annotated.html", "annotated" ],
-    [ "Download", "^https://github.com/xicilion/fibjs", null ],
-    [ "Forum", "^http://baoz.cn/fibjs", null ],
-    [ "中文版", "../index.html", null ]
-  ] ]
-];
-
-var NAVTREEINDEX =
-[
-"../index.html",
-"d2/d68/interfaceMySQL.html#ab5ddefb9b2c6c1a0cdfdac90e13de736",
-"d4/dd8/interfaceXmlCDATASection.html#a2e8f8f0fe64a8843d0b976714deed4b0",
-"d6/df7/interfaceTrigger.html#ad66be13eb5b7c5f560fd8511565138c4",
-"d8/d2c/interfaceDbConnection.html#ab5ddefb9b2c6c1a0cdfdac90e13de736",
-"da/df6/interfaceXmlProcessingInstruction.html#ac61f07a7d6c5471293371086af3186bd",
-"de/da6/interfaceXmlNodeList.html"
-];
-
-var SYNCONMSG = 'click to disable panel synchronisation';
-var SYNCOFFMSG = 'click to enable panel synchronisation';
 var navTreeSubIndices = new Array();
 
 function getData(varName)
@@ -162,7 +105,7 @@ function createIndent(o,domNode,node,level)
     node.expandToggle.onclick = function() {
       if (node.expanded) {
         $(node.getChildrenUL()).slideUp("fast");
-        node.plus_img.src = node.relpath+"ftv2pnode.png";
+        node.plus_img.src = node.relpath+"arrowright.png";
         node.expanded = false;
       } else {
         expandNode(o, node, false, false);
@@ -170,7 +113,7 @@ function createIndent(o,domNode,node,level)
     }
     node.expandToggle.appendChild(imgNode);
     domNode.appendChild(node.expandToggle);
-    imgNode.src = node.relpath+"ftv2pnode.png";
+    imgNode.src = node.relpath+"arrowright.png";
   } else {
     var span = document.createElement("span");
     span.style.display = 'inline-block';
@@ -326,9 +269,9 @@ function expandNode(o, node, imm, showRoot)
         $(node.getChildrenUL()).slideDown("fast");
       }
       if (node.isLast) {
-        node.plus_img.src = node.relpath+"ftv2mlastnode.png";
+        node.plus_img.src = node.relpath+"arrowdown.png";
       } else {
-        node.plus_img.src = node.relpath+"ftv2mnode.png";
+        node.plus_img.src = node.relpath+"arrowdown.png";
       }
       node.expanded = true;
     }
@@ -398,11 +341,7 @@ function showNode(o, node, index, hash)
         getNode(o, node);
       }
       $(node.getChildrenUL()).css({'display':'block'});
-      if (node.isLast) {
-        node.plus_img.src = node.relpath+"ftv2mlastnode.png";
-      } else {
-        node.plus_img.src = node.relpath+"ftv2mnode.png";
-      }
+      node.plus_img.src = node.relpath+"arrowdown.png";
       node.expanded = true;
       var n = node.children[o.breadcrumbs[index]];
       if (index+1<o.breadcrumbs.length) {
@@ -540,7 +479,7 @@ function initNavTree(toroot,relpath)
   o.node.expanded = false;
   o.node.isLast = true;
   o.node.plus_img = document.createElement("img");
-  o.node.plus_img.src = relpath+"ftv2pnode.png";
+  o.node.plus_img.src = relpath+"arrowright.png";
   o.node.plus_img.width = 16;
   o.node.plus_img.height = 22;
 
---
+++ html/en/navtreedata.js
@@ -0,0 +1,57 @@
+var NAVTREE =
+[
+  [ "Javascript on Fiber", "index.html", [
+    [ "Quick Start", "index.html", [
+      [ "What’s fibjs", "index.html#intro_sec", null ],
+      [ "Build release", "index.html#install_sec", [
+        [ "Step 1: Preparation for Building", "index.html#install_step1", null ],
+        [ "Step 2: Fetch source code", "index.html#install_step2", null ],
+        [ "Step 3: Building", "index.html#install_step3", null ],
+        [ "Step 4: Test cases", "index.html#install_step4", null ],
+        [ "Step 5: Have a rest and drinking", "index.html#install_step5", null ]
+      ] ]
+    ] ],
+    [ "Beginning", "d2/de8/start_prog.html", [
+      [ "Beginning", "d2/de8/start_prog.html#program_sec", [
+        [ "Step 1: Hello, World!", "d2/de8/start_prog.html#prg_step1", null ],
+        [ "Step 2: DANG, DANG, DANG...", "d2/de8/start_prog.html#prg_step2", null ],
+        [ "Step 3: DANGs", "d2/de8/start_prog.html#prg_step3", null ],
+        [ "Advance: Parallel Jobs", "d2/de8/start_prog.html#prg_step4", null ],
+        [ "More Advance: Asynchronous Jobs", "d2/de8/start_prog.html#prg_step5", null ],
+        [ "Summary", "d2/de8/start_prog.html#prg_step10", null ]
+      ] ]
+    ] ],
+    [ "Module Management", "d0/db5/module.html", [
+      [ "Basic Elements", "d0/db5/module.html#element", [
+        [ "require() Function", "d0/db5/module.html#fun_require", null ],
+        [ "define() Function", "d0/db5/module.html#fun_define", null ],
+        [ "module Object", "d0/db5/module.html#obj_module", null ],
+        [ "exports Object", "d0/db5/module.html#obj_exports", null ]
+      ] ]
+    ] ],
+    [ "Server Programming", "da/db3/server.html", [
+      [ "Static File Server", "da/db3/server.html#web_file", null ],
+      [ "Dynamic Server", "da/db3/server.html#web_script", null ],
+      [ "Both Static and Dynamic Supoort", "da/db3/server.html#web_both", null ]
+    ] ],
+    [ "Module", "namespaces.html", "namespaces" ],
+    [ "Object", "annotated.html", "annotated" ],
+    [ "Download", "^https://github.com/xicilion/fibjs", null ],
+    [ "Forum", "^http://baoz.cn/fibjs", null ],
+    [ "中文版", "../index.html", null ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"../index.html",
+"d2/d68/interfaceMySQL.html#ab5ddefb9b2c6c1a0cdfdac90e13de736",
+"d4/dd8/interfaceXmlCDATASection.html#a2e8f8f0fe64a8843d0b976714deed4b0",
+"d6/df7/interfaceTrigger.html#ad66be13eb5b7c5f560fd8511565138c4",
+"d8/d2c/interfaceDbConnection.html#ab5ddefb9b2c6c1a0cdfdac90e13de736",
+"da/df6/interfaceXmlProcessingInstruction.html#ac61f07a7d6c5471293371086af3186bd",
+"de/da6/interfaceXmlNodeList.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
