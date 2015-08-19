89752db3993f3b45e55f4bddcf26d7812266a633
响马 <lion@9465.net>
2015-08-19 14:53:27 +0800

    生成新的更新文档

--- html/en/de/d09/interfaceHeapSnapshot.js
+++ html/en/de/d09/interfaceHeapSnapshot.js
@@ -2,6 +2,7 @@ var interfaceHeapSnapshot =
 [
     [ "diff", "de/d09/interfaceHeapSnapshot.html#af6eba58a29be9c3dafe30fe6ef281913", null ],
     [ "getNodeById", "de/d09/interfaceHeapSnapshot.html#af00405058ecc8b96d4f09b556d2c4861", null ],
+    [ "save", "de/d09/interfaceHeapSnapshot.html#aa4f17a9e85bf99789d8df12c5d29cca5", null ],
     [ "dispose", "de/d09/interfaceHeapSnapshot.html#a6b88dbc459449d2280338e5b702a68b4", null ],
     [ "toString", "de/d09/interfaceHeapSnapshot.html#ac61f07a7d6c5471293371086af3186bd", null ],
     [ "toJSON", "de/d09/interfaceHeapSnapshot.html#ad66be13eb5b7c5f560fd8511565138c4", null ],
