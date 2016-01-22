4dfcf7d3b6f51c459715fbc08d613132c8a9909d
响马 <lion@9465.net>
2016-01-22 17:08:53 +0800

    更新文档

--- html/en/navtreedata.js
+++ html/en/navtreedata.js
@@ -11,28 +11,28 @@ var NAVTREE =
         [ "Step 5: Have a rest and drinking", "index.html#install_step5", null ]
       ] ]
     ] ],
-    [ "Beginning", "d2/de8/start_prog.html", [
-      [ "Beginning", "d2/de8/start_prog.html#program_sec", [
-        [ "Step 1: Hello, World!", "d2/de8/start_prog.html#prg_step1", null ],
-        [ "Step 2: DANG, DANG, DANG...", "d2/de8/start_prog.html#prg_step2", null ],
-        [ "Step 3: DANGs", "d2/de8/start_prog.html#prg_step3", null ],
-        [ "Advance: Parallel Jobs", "d2/de8/start_prog.html#prg_step4", null ],
-        [ "More Advance: Asynchronous Jobs", "d2/de8/start_prog.html#prg_step5", null ],
-        [ "Summary", "d2/de8/start_prog.html#prg_step10", null ]
+    [ "Beginning", "start_prog.html", [
+      [ "Beginning", "start_prog.html#program_sec", [
+        [ "Step 1: Hello, World!", "start_prog.html#prg_step1", null ],
+        [ "Step 2: DANG, DANG, DANG...", "start_prog.html#prg_step2", null ],
+        [ "Step 3: DANGs", "start_prog.html#prg_step3", null ],
+        [ "Advance: Parallel Jobs", "start_prog.html#prg_step4", null ],
+        [ "More Advance: Asynchronous Jobs", "start_prog.html#prg_step5", null ],
+        [ "Summary", "start_prog.html#prg_step10", null ]
       ] ]
     ] ],
-    [ "Module Management", "d0/db5/module.html", [
-      [ "Basic Elements", "d0/db5/module.html#element", [
-        [ "require() Function", "d0/db5/module.html#fun_require", null ],
-        [ "define() Function", "d0/db5/module.html#fun_define", null ],
-        [ "module Object", "d0/db5/module.html#obj_module", null ],
-        [ "exports Object", "d0/db5/module.html#obj_exports", null ]
+    [ "Module Management", "module.html", [
+      [ "Basic Elements", "module.html#element", [
+        [ "require() Function", "module.html#fun_require", null ],
+        [ "define() Function", "module.html#fun_define", null ],
+        [ "module Object", "module.html#obj_module", null ],
+        [ "exports Object", "module.html#obj_exports", null ]
       ] ]
     ] ],
-    [ "Server Programming", "da/db3/server.html", [
-      [ "Static File Server", "da/db3/server.html#web_file", null ],
-      [ "Dynamic Server", "da/db3/server.html#web_script", null ],
-      [ "Both Static and Dynamic Supoort", "da/db3/server.html#web_both", null ]
+    [ "Server Programming", "server.html", [
+      [ "Static File Server", "server.html#web_file", null ],
+      [ "Dynamic Server", "server.html#web_script", null ],
+      [ "Both Static and Dynamic Supoort", "server.html#web_both", null ]
     ] ],
     [ "Module", "namespaces.html", "namespaces" ],
     [ "Object", "annotated.html", "annotated" ],
@@ -45,12 +45,12 @@ var NAVTREE =
 var NAVTREEINDEX =
 [
 "../index.html",
-"d2/d68/interfaceMySQL.html#a4f19b7fb871da01a3133649c27797a83",
-"d4/dc7/interfaceStream.html#ad66be13eb5b7c5f560fd8511565138c4",
-"d6/df7/interfaceTrigger.html#a1227f3a0cfeb5be138deba4e21fa42eb",
-"d7/df5/interfaceSQLite.html#ae6d31f438b2c274f32aade526f92da2f",
-"da/de8/interfaceSmtp.html#a01a00f4b52964eec188c5681ec74758b",
-"dd/d94/interfaceSocket.html#a84ed037fcf775c6c38877da2626bdbbc"
+"d2/d68/interfaceMySQL.html#ab9261a7662bc8bb4a898bc15dc16f206",
+"d4/dd8/interfaceXmlCDATASection.html#a7144e0d715d39be72d7c14008fb1d74e",
+"d7/d32/interfaceRedis.html#a04f350bace05c348daed5b1bdeb0b12e",
+"d8/d89/namespacevm.html",
+"da/df6/interfaceXmlProcessingInstruction.html#a1e2d8e642e8987ccfba221bed65fecf0",
+"dd/dc1/interfaceXmlComment.html#a1e2d8e642e8987ccfba221bed65fecf0"
 ];
 
 var SYNCONMSG = 'click to disable panel synchronisation';
--- html/search/all_2.js
+++ html/search/all_2.js
@@ -7,11 +7,7 @@ var searchData=
   ['badcert_5fnot_5ftrusted',['BADCERT_NOT_TRUSTED',['../df/dd2/namespacessl.html#aabc1deedebc1e64e686b0caead187884',1,'ssl']]],
   ['badcert_5frevoked',['BADCERT_REVOKED',['../df/dd2/namespacessl.html#a2c2535a0b748318087c99cfdc12c2cc6',1,'ssl']]],
   ['base32',['base32',['../d4/dba/namespacebase32.html',1,'base32'],['../db/da8/namespaceencoding.html#a86bde7f64f9870f2de7c7cdaa3da6ad3',1,'encoding::base32()']]],
-  ['base32decode',['base32Decode',['../db/da8/namespaceencoding.html#a2e33cdcfde3b6af45ed2d341fbb72ffd',1,'encoding']]],
-  ['base32encode',['base32Encode',['../db/da8/namespaceencoding.html#a28a31f8be7e89ab2e0cb5f374cb0288c',1,'encoding']]],
   ['base64',['base64',['../d5/d37/namespacebase64.html',1,'base64'],['../d9/dae/interfaceBuffer.html#a36df68fd3011d583c237346289fe22d3',1,'Buffer::base64()'],['../db/da8/namespaceencoding.html#a8081785db1dfad236b086d4e2a9c79ba',1,'encoding::base64()']]],
-  ['base64decode',['base64Decode',['../db/da8/namespaceencoding.html#a49935417ee1734f4c8008cbf9cef37db',1,'encoding']]],
-  ['base64encode',['base64Encode',['../db/da8/namespaceencoding.html#ac01212b33bc396f9b56d5ead42d9d1dc',1,'encoding']]],
   ['base64vlq',['base64vlq',['../de/d5c/namespacebase64vlq.html',1,'base64vlq'],['../db/da8/namespaceencoding.html#a7b9b39387963aca4019374e72257d87d',1,'encoding::base64vlq()']]],
   ['basename',['basename',['../d0/d43/namespacepath.html#a82751cb3c3cda3cbd756667343737f4d',1,'path']]],
   ['bdd',['BDD',['../df/d04/namespacetest.html#ae4137e9ca407ce2fe025a9b61cabe1cd',1,'test']]],
@@ -36,9 +32,7 @@ var searchData=
   ['both',['BOTH',['../dd/d5d/namespacegd.html#a777685b5adc4abb88c9acede6cc66c0e',1,'gd']]],
   ['brightness',['BRIGHTNESS',['../dd/d5d/namespacegd.html#aadea7459b8ff35122c1b45980da86eea',1,'gd']]],
   ['bson',['bson',['../d7/d28/namespacebson.html',1,'bson'],['../db/da8/namespaceencoding.html#a4ad734b31015e44f43735c15e54f5c5e',1,'encoding::bson()']]],
-  ['bsondecode',['bsonDecode',['../db/da8/namespaceencoding.html#a6e604f8a279f13c325ee0041f4583130',1,'encoding']]],
-  ['bsonencode',['bsonEncode',['../db/da8/namespaceencoding.html#a11c73742452c48101c5887a9bbfbb67c',1,'encoding']]],
-  ['buffer',['Buffer',['../d9/dae/interfaceBuffer.html',1,'Buffer'],['../d9/dae/interfaceBuffer.html#af19634a31b59ee8f2f4031715bf89952',1,'Buffer::Buffer(Array datas)'],['../d9/dae/interfaceBuffer.html#ac16e65f2c0d120d046cb02cc1841c17d',1,'Buffer::Buffer(Buffer buffer)'],['../d9/dae/interfaceBuffer.html#ab9e33ff0da7a8dd5c812330c1ed08c17',1,'Buffer::Buffer(String str, String codec=&quot;utf8&quot;)'],['../d9/dae/interfaceBuffer.html#a09fe57b4916e313b00c53a215445e469',1,'Buffer::Buffer(Integer size=0)'],['../d4/dfd/namespaceglobal.html#abe961e24f94e940799497f08f5704916',1,'global::Buffer()']]],
+  ['buffer',['Buffer',['../d9/dae/interfaceBuffer.html',1,'Buffer'],['../d9/dae/interfaceBuffer.html#af19634a31b59ee8f2f4031715bf89952',1,'Buffer::Buffer(Array datas)'],['../d9/dae/interfaceBuffer.html#a35e7f5e09a2e2976d2f9344459101b0a',1,'Buffer::Buffer(ArrayBuffer datas)'],['../d9/dae/interfaceBuffer.html#af609ff01a079a58c39b3fc72a5528e05',1,'Buffer::Buffer(TypedArray datas)'],['../d9/dae/interfaceBuffer.html#ac16e65f2c0d120d046cb02cc1841c17d',1,'Buffer::Buffer(Buffer buffer)'],['../d9/dae/interfaceBuffer.html#ab9e33ff0da7a8dd5c812330c1ed08c17',1,'Buffer::Buffer(String str, String codec=&quot;utf8&quot;)'],['../d9/dae/interfaceBuffer.html#a09fe57b4916e313b00c53a215445e469',1,'Buffer::Buffer(Integer size=0)'],['../d4/dfd/namespaceglobal.html#abe961e24f94e940799497f08f5704916',1,'global::Buffer()']]],
   ['bufferedstream',['BufferedStream',['../d0/d37/interfaceBufferedStream.html',1,'BufferedStream'],['../d0/d37/interfaceBufferedStream.html#a6dde4ed97e788d9a9963b3548d1a837b',1,'BufferedStream::BufferedStream()'],['../d1/d96/namespaceio.html#a555ba063f09dc4745b6beb5a91395df5',1,'io::BufferedStream()']]],
   ['buildinfo',['buildInfo',['../d6/d72/namespaceutil.html#a38b22f190ce11008314b51470042ce2b',1,'util']]]
 ];
