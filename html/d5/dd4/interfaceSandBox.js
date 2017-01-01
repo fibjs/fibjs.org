a6f96d83b8d116e1530e03c5d332a8bed4374cb1
响马 <lion@9465.net>
2017-01-02 00:38:36 +0800

    更新文档

--- html/d5/dd4/interfaceSandBox.js
+++ html/d5/dd4/interfaceSandBox.js
@@ -10,7 +10,7 @@ var interfaceSandBox =
     [ "remove", "d5/dd4/interfaceSandBox.html#a0ee2b7c07ba2ac80f043a36a401fa72b", null ],
     [ "clone", "d5/dd4/interfaceSandBox.html#a581daffaf92d0b1266c60f100a66ad40", null ],
     [ "run", "d5/dd4/interfaceSandBox.html#a067679bd863305a489058868411a07f1", null ],
-    [ "require", "d5/dd4/interfaceSandBox.html#a134871a1c0b138d7b582225810ed6e9e", null ],
+    [ "require", "d5/dd4/interfaceSandBox.html#a8208bd9c2bc5300f73ef7986acda925c", null ],
     [ "dispose", "d5/dd4/interfaceSandBox.html#a6b88dbc459449d2280338e5b702a68b4", null ],
     [ "equals", "d5/dd4/interfaceSandBox.html#a94c197c586ecfcb7cb733e0d543ce5ad", null ],
     [ "toString", "d5/dd4/interfaceSandBox.html#ac61f07a7d6c5471293371086af3186bd", null ],
