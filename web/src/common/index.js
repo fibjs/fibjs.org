1b9b238bba889e92dfff38ecfb367bb0ce1eadf2
响马 <lion@9465.net>
2017-06-18 22:32:30 +0800

    增加统计

--- web/src/common/index.js
+++ web/src/common/index.js
@@ -10,3 +10,11 @@ if ((!document.addEventListener)) {
 import 'jquery';
 import 'bootstrap';
 import './ie10-viewport';
+
+var _hmt = _hmt || [];
+(function () {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?90aebbd2b5f574be23bf8fb84c1edf91";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
