c314e2457365726104d014ed29b229e1dba13560
响马 <lion@9465.net>
2017-06-20 10:32:13 +0800

    toc 跟踪滚动

--- web/src/pages/docs.js
+++ web/src/pages/docs.js
@@ -5,25 +5,54 @@ import './docs.css';
 
 //Executes your code when the DOM is ready.  Acts the same as $(document).ready().
 $(function () {
+    var _toc = $("#toc");
+
     //Calls the tocify method on your HTML div.
-    $("#toc").tocify({
+    _toc.tocify({
         showAndHide: false,
         extendPage: false
     });
 
+    var _content = $(".content");
+    var _window = $(window);
+    var _document = $(document);
+
     var fixed = false;
-    var _top = $("#toc").offset().top;
+    var _height = _toc.height() + 50;
+    var _top = _toc.offset().top;
+    var t_top = -1;
 
-    $(window).scroll(function () {
-        var st = $(window).scrollTop();
+    function to(t) {
+        t = Math.floor(t);
+        if (t_top !== t) {
+            t_top = t;
+            _toc.css('top', t + 'px');
+        }
+    }
 
-        if ((st > _top) !== fixed) {
+    function _sync() {
+        var _scroll_top = _window.scrollTop();
+        var w_height = _window.height();
+        var c_height = _content.height();
+
+        if ((_scroll_top > _top) !== fixed) {
             fixed = !fixed;
             if (fixed)
-                $("#toc").addClass('fixed_tocify');
+                _toc.addClass('fixed_tocify');
             else
-                $("#toc").removeClass('fixed_tocify');
-
+                _toc.removeClass('fixed_tocify');
         }
-    })
+
+        if (fixed) {
+            if (_height < w_height)
+                to(0);
+            else
+                to(-((_scroll_top - _top) / (c_height - w_height) * (_height - w_height)));
+        }
+    }
+
+    _sync();
+
+    _window.resize(_sync);
+    _window.scroll(_sync);
 });
