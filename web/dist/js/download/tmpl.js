278a2a188e469909b52d6bb7eb428091c6db23c2
响马 <lion@9465.net>
2017-06-21 23:43:41 +0800

    增加下载事件

--- web/dist/js/download/tmpl.js
+++ web/dist/js/download/tmpl.js
@@ -1,1 +1,1 @@
-webpackJsonp([1],[function(c,n,s){"use strict";s(2)}]);
+webpackJsonp([1],[function(n,c,t){(function(n){"use strict";t(2),n(".down").click(function(){ga("send","event","下载",this.innerText)})}).call(c,t(1))}]);
--- web/src/pages/download/tmpl.js
+++ web/src/pages/download/tmpl.js
@@ -1,1 +1,5 @@
 import 'common';
+
+$(".down").click(function () {
+    ga('send', 'event', '下载', this.innerText);
+});
