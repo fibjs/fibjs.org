08893b6c386f485f259363bcdbbd94918852c1de
响马 <lion@9465.net>
2017-06-21 22:13:45 +0800

    修改为谷歌统计

--- web/src/common/index.js
+++ web/src/common/index.js
@@ -35,10 +35,17 @@ $(document).ready(function () {
 
 });
 
-var _hmt = _hmt || [];
-(function () {
-    var hm = document.createElement("script");
-    hm.src = "https://hm.baidu.com/hm.js?90aebbd2b5f574be23bf8fb84c1edf91";
-    var s = document.getElementsByTagName("script")[0];
-    s.parentNode.insertBefore(hm, s);
-})();
+(function (i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r;
+    i[r] = i[r] || function () {
+        (i[r].q = i[r].q || []).push(arguments)
+    }, i[r].l = 1 * new Date();
+    a = s.createElement(o),
+        m = s.getElementsByTagName(o)[0];
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore(a, m)
+})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+ga('create', 'UA-101423781-1', 'auto');
+ga('send', 'pageview');
