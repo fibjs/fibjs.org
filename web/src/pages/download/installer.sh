339bab3821feb57416cb0d31aeb33c40c4c02458
xicilion <lion@9465.net>
2023-02-13 00:22:33 +0800

    support loong64 installer.

--- web/src/pages/download/installer.sh
+++ web/src/pages/download/installer.sh
@@ -6,13 +6,11 @@ HOST_ARCH=`uname -m`
 case ${HOST_ARCH} in
 	i386|i686) HOST_ARCH="x86";;
 	x86_64|amd64) HOST_ARCH="x64";;
-	armv6) HOST_ARCH="armv6";;
 	armv7|armv7s|armv7l) HOST_ARCH="arm";;
 	aarch64|arm64) HOST_ARCH="arm64";;
-	mips|mipsel) HOST_ARCH="mips";;
 	mips64) HOST_ARCH="mips64";;
-	powerpc) HOST_ARCH="ppc";;
 	ppc64) HOST_ARCH="ppc64";;
+	loongarch64) HOST_ARCH="loong64";;
 esac
 
 case ${HOST_OS} in
@@ -23,6 +21,7 @@ esac
 
 VERSION=`curl -s http://fibjs.org/dist/version.txt`
 
-curl http://fibjs.org/dist/${VERSION}/installer-${VERSION}-${HOST_OS}-${HOST_ARCH}.sh >> installer.sh
-sh installer.sh
-rm installer.sh
+echo "Downloading fibjs ${VERSION} for ${HOST_OS} ${HOST_ARCH} ..."
+curl -s http://fibjs.org/dist/${VERSION}/installer-${VERSION}-${HOST_OS}-${HOST_ARCH}.sh >> installer-${VERSION}-${HOST_OS}-${HOST_ARCH}.sh
+sh installer-${VERSION}-${HOST_OS}-${HOST_ARCH}.sh
+rm installer-${VERSION}-${HOST_OS}-${HOST_ARCH}.sh
